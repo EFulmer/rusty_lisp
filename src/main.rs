a28398097a131082f5253615e3ef26cf6dd16749
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-05-01 01:26:19 -0400

    added first ver tokenizer -- big err to fix later

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-fn tokenize(program: String) -> Vec<String> { 
+fn tokenize(program: &String) -> Vec<String> { 
     program.replace("(", " ( ").replace(")", " ) ")
            .split_whitespace().map(|x| x.to_string())
            .collect()
+}
+
+fn read_from_tokens(tokens: &mut Vec<String>) -> Result<Vec<LispVal>, String> {
+    let res: Result<Vec<LispVal>, String>;
+    if tokens.len() == 0 {
+        res = Err("Unexpected EOF while reading.".to_string());
+    } else {
+
+        let token = tokens.remove(0); // ok because ^ 
+
+        if "(" == token {
+            let mut vals = Vec::new();
+
+            while tokens.get(0).unwrap() != ")" { // this line is probably Bad
+                vals.append(&mut read_from_tokens(tokens).unwrap()); // TODO this line is probably Bad too.
+            }
+            tokens.pop(); // TODO -- this is the line that catches missing rparens. Try tokens.remove(0) wrapped in something that'll return a Result?
+
+            let l = LispVal::LispList { l: Box::new(vals) };
+            res = Ok(vec![l]);
+        }
+        else if ")" == token {
+            res = Err("Unexpected rparen found.".to_string());
+        } else {
+            let l = vec![LispVal::atom(token)];
+            res = Ok(l);
+        }
+    }
+    res
+}
+
+fn parse(program: &String) -> Result<Vec<LispVal>, String> {
+    let mut tokens = tokenize(program);
+    read_from_tokens(&mut tokens)
 }
 
 #[derive(Debug)]
@@ -27,7 +61,21 @@ impl LispVal {
 
 fn main() {
     println!("Hello, world!");
+
     let s = "(+ 1 1)".to_string();
     println!("Let's tokenize {}", s);
-    println!("{:?}", tokenize(s));
+    println!("{:?}", tokenize(&s));
+    println!("Let's parse {} into a Lisp-y form.", s);
+    println!("{:?}", parse(&s));
+
+    let s2 = "(if (< x 0) (* x -1) x)".to_string();
+    println!("Let's try something more complex: {}", s2);
+    println!("Tokenized: {:?}", tokenize(&s2));
+    println!("Let's parse it now.");
+    println!("Parsed: {:?}", parse(&s2));
+
+    let s3 = "(if (< x 0) (* x -1) x".to_string();
+    println!("This test should fail-- uneven parentheses! {:?}", s3);
+    println!("Parsed: {:?}", parse(&s3));
+    println!("It doesn't. Gotta fix this later.");
 }
