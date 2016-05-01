b4c1ca7d5183be6840409cf2f78e93f31dd4caea
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-05-01 00:49:23 -0400

    tokenize fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn tokenize(program: String) -> Vec<String> { 
+    program.replace("(", " ( ").replace(")", " ) ")
+           .split_whitespace().map(|x| x.to_string())
+           .collect()
+}
+
 fn main() {
     println!("Hello, world!");
+    let s = "(+ 1 1)".to_string();
+    println!("Let's tokenize {}", s);
+    println!("{:?}", tokenize(s));
 }
