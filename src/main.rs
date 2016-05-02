89c066a0d839941f1bf56555e1090debcd0ac389
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-05-02 03:45:55 -0400

    first draft of add fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::iter::Iterator;
+use LispVal::*;
 
 fn tokenize(program: &String) -> Vec<String> { 
     program.replace("(", " ( ").replace(")", " ) ")
@@ -45,12 +47,13 @@ fn parse(program: &String) -> Result<Vec<LispVal>, String> {
     read_from_tokens(&mut tokens)
 }
 
-#[derive(Debug)]
+// #[derive(Debug)]
 enum LispVal {
-    LispInt { x: i32 },
-    LispFlt { x: f64 },
-    LispSym { s: String },
-    LispList { l: Box<Vec<LispVal>> },
+    LispInt     { x: i32                        },
+    LispFlt     { x: f64                        },
+    LispSym     { s: String                     },
+    LispList    { l: Box<Vec<LispVal>>          },
+    LispFn      { f: Box<Fn(LispVal) -> LispVal>  },
 }
 
 impl LispVal {
@@ -63,6 +66,70 @@ impl LispVal {
                 Err(_) => LispVal::LispSym { s: s },
             }
         }
+    }
+}
+
+fn standard_env() -> HashMap<String, LispVal> {
+    let mut env = HashMap::new();
+
+    env.insert("+", Box::new(lisp_add));
+    env;
+}
+
+fn only_lisp_numbers(v: &Vec<LispVal>) -> bool {
+    (*v).into_iter().all(|x| match x { 
+        LispInt { .. } => true,
+        LispFlt { .. } => true, 
+        _              => false,
+    })
+}
+
+fn contains_floats(v: &Vec<LispVal>) -> bool {
+    (*v).into_iter().any(|x| match x {
+        LispFlt { .. }  => true,
+        _               => false,
+    })
+}
+
+fn convert_to_floats(v: &Vec<LispVal>) -> Vec<LispVal> {
+    (*v).into_iter()
+     .map(|x| match x {
+         LispInt { x }   => LispFlt { x: x as f64 },
+         LispFlt { ..  } => x,
+         _               => panic!("shouldn't happen -- called in wrong order")})
+     .collect()
+}
+
+// TODO -- should I implement Add?
+fn sum_list_int(v: &Vec<LispVal>) -> LispVal { 
+    (*v).into_iter()
+        .fold(LispInt { x : 0 }, 
+              |LispInt { x : sum }, LispInt { x }| LispInt { x: sum + x})
+}
+
+fn sum_list_float(v: &Vec<LispVal>) -> LispVal { 
+    (*v).into_iter()
+        .fold(LispFlt { x : 0.0 }, 
+              |LispFlt { x : sum }, LispFlt { x }| LispFlt { x: sum + x})
+}
+
+fn lisp_add(v: LispVal) -> Result<LispVal, String> {
+    match v {
+        LispInt     { .. } => Ok(v),
+        LispFlt     { .. } => Ok(v),
+        LispList    { l }  => { if only_lisp_numbers(&l) {
+                                    if contains_floats(&l) {
+                                        let v = convert_to_floats(&l);
+                                        Ok(sum_list_float(&v))
+                                    } else {
+                                        Ok(sum_list_int(&l))
+                                    }
+                                } else { 
+                                    Err("found non-numerics in arg list".to_string())
+                                }
+                            }
+                            // panic!("not implemented yet lol"),
+        _                  => Err("Expected list of numeric values.".to_string()),
     }
 }
 
@@ -83,6 +150,6 @@ fn main() {
 
     let s3 = "(if (< x 0) (* x -1) x".to_string();
     println!("This test should fail-- uneven parentheses! {:?}", s3);
-    // println!("Parsed: {:?}", parse(&s3));
-    // println!("It doesn't. Gotta fix this later.");
+    println!("Parsed: {:?}", parse(&s3));
+    println!("It doesn't. Gotta fix this later.");
 }
