36aac2c74d3f7d7f94ff6ccca074284b05b9b8bd
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-05-03 13:35:05 -0400

    rename LispFlt to LispFloat

--- src/types.rs
+++ src/types.rs
@@ -8,15 +8,15 @@ pub type Env = HashMap<String, LispVal>;
 #[derive(Debug)]
 pub enum LispVal {
     /// An integral numeric value. Currently 32-bit precision.
-    LispInt     { x: i32                        },
+    LispInt   { x: i32                        },
     /// A floating-point numeric value. Currently 64-bit precision; precision 
     /// unlikely to change.
-    LispFlt     { x: f64                        },
+    LispFloat { x: f64                        },
     /// Symbol, used for representing both language constructs like `if`, `define`, 
     /// `lambda`, and user-defined symbols of the form `'sym-name`.
-    LispSym     { s: String                     },
+    LispSym   { s: String                     },
     /// Lisp list: a reference to a list containing more Lisp values.
-    LispList    { l: Box<Vec<LispVal>>          },
+    LispList  { l: Box<Vec<LispVal>>          },
     // Reference to a function on Lisp values. Will include function metadata 
     // (captured values, etc.) in future version.
     // LispFn      { f: Box<Fn(LispVal) -> LispVal>  },
@@ -28,7 +28,7 @@ impl LispVal {
         match s.parse::<i32>() {
             Ok(x) => LispVal::LispInt { x: x } ,
             Err(_) => match s.parse::<f64>() {
-                Ok(x) => LispVal::LispFlt { x: x},
+                Ok(x) => LispVal::LispFloat { x: x},
                 Err(_) => LispVal::LispSym { s: s },
             }
         }
